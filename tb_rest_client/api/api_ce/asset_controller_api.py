917bdce64b6043b13b654cf9823f88b89ee64d74
samson0v <vitalikbidochka@gmail.com>
2021-07-20 12:29:55 +0300

    Added assing_asset_to_edge api function for api_ce

--- tb_rest_client/api/api_ce/asset_controller_api.py
+++ tb_rest_client/api/api_ce/asset_controller_api.py
@@ -837,7 +837,8 @@ class AssetControllerApi(object):
         if kwargs.get('async_req'):
             return self.unassign_asset_from_edge_using_delete_with_http_info(edge_id, asset_id, **kwargs)  # noqa: E501
         else:
-            (data) = self.unassign_asset_from_edge_using_delete_with_http_info(edge_id, asset_id, **kwargs)  # noqa: E501
+            (data) = self.unassign_asset_from_edge_using_delete_with_http_info(edge_id, asset_id,
+                                                                               **kwargs)  # noqa: E501
             return data
 
     def unassign_asset_from_edge_using_delete_with_http_info(self, edge_id, asset_id, **kwargs):
@@ -914,6 +915,110 @@ class AssetControllerApi(object):
             post_params=form_params,
             files=local_var_files,
             response_type=None,  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
+    def assign_asset_to_edge_using_post(self, edge_id, asset_id, **kwargs):
+        """assignAssetToEdge  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api_pe.assign_asset_to_edge_using_post(edge_id, asset_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str edge_id: edgeId (required)
+        :param str asset_id: assetId (required)
+        :return: Asset
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.assign_asset_to_edge_using_post_with_http_info(edge_id, asset_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.assign_asset_to_edge_using_post_with_http_info(edge_id, asset_id, **kwargs)  # noqa: E501
+            return data
+
+    def assign_asset_to_edge_using_post_with_http_info(self, edge_id, asset_id, **kwargs):
+        """assignAssetToEdge  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api_pe.assign_asset_to_edge_using_post_with_http_info(edge_id, asset_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str edge_id: edgeId (required)
+        :param str asset_id: assetId (required)
+        :return: Asset
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['edge_id', 'asset_id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            params[key] = val
+        del params['kwargs']
+
+        # verify the required parameter 'edge_id' is set
+        if ('edge_id' not in params or
+                params['edge_id'] is None):
+            raise ValueError(
+                "Missing the required parameter `edge_id` when calling `unassign_asset_from_edge_using_delete`")  # noqa: E501
+        # verify the required parameter 'asset_id' is set
+        if ('asset_id' not in params or
+                params['asset_id'] is None):
+            raise ValueError(
+                "Missing the required parameter `asset_id` when calling `delete_asset_using_delete`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'edge_id' in params:
+            path_params['edgeId'] = params['edge_id']  # noqa: E501
+        if 'asset_id' in params:
+            path_params['assetId'] = params['asset_id']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['*/*'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['X-Authorization']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/api/edge/{edgeId}/asset/{assetId}', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='Asset',  # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
--- tb_rest_client/rest_client_base.py
+++ tb_rest_client/rest_client_base.py
@@ -175,6 +175,10 @@ class RestClientBase(Thread):
         asset_id = self.get_id(asset_id)
         return self.asset_controller.unassign_asset_from_edge_using_delete(edge_id, asset_id)
 
+    def assign_asset_to_edge(self, edge_id: str, asset_id: AssetId):
+        asset_id = self.get_id(asset_id)
+        return self.asset_controller.assign_asset_to_edge_using_post(edge_id, asset_id)
+
     def get_tenant_assets(self, page_size=10, page=0, type=None, text_search=None, sort_property=None, sort_order=None,
                           limit=100000):
         return self.asset_controller.get_tenant_assets_using_get(page_size=str(page_size),
